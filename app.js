03b2c4d4794fa5b45f9d36dcf39b0c8d35a9de91
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2020-12-26 15:48:32 -0500

    added first questions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-console.log(inquirer)
+
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
@@ -10,3 +10,15 @@ console.log(inquirer)
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+
+
+    }
+])
+.then(answers => console.log(answers))
